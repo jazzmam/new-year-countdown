5772a525f4ee599cc74f041ac4fdc63ad0b636fc
Ruta <rutakalytyte@RutaKalytyteMBP.lan>
2020-12-27 19:51:28 +0200

    .

--- script.js
+++ script.js
@@ -1,1 +1,7 @@
-console.log("dfdfd");
+console.log("WORKS");
+
+const days = document.getElementById('days');
+const hours = document.getElementById('hours');
+const minutes = document.getElementById('minutes');
+const seconds = document.getElementById('seconds');
+const coutdown = document.getElementById('coutdown');
